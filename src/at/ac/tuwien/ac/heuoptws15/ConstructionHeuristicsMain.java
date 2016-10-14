810d02d6b4ccbf69a0fa22ee76e6695eb0f6ea41
Martin318 <martinbloeschl@gmail.com>
2016-10-14 10:54:50 +0200

    Initial Commit

---
+++ src/at/ac/tuwien/ac/heuoptws15/ConstructionHeuristicsMain.java
@@ -0,0 +1,14 @@
+package at.ac.tuwien.ac.heuoptws15;
+
+/**
+ * Created by Martin on 14.10.2016.
+ */
+public class ConstructionHeuristicsMain {
+
+    public static void main(String[] args){
+
+        System.out.println("Test");
+
+    }
+
+}
