8b577f664814d403ac472ffe09505c0d0e8de1e2
Mivey <mivster@ymail.com>
2016-10-27 18:25:29 +0200

    Requirements for a working neighbourhood structure

---
+++ src/at/ac/tuwien/ac/heuoptws15/Neighbourhood.java
@@ -0,0 +1,11 @@
+package at.ac.tuwien.ac.heuoptws15;
+
+import java.util.List;
+
+/**
+ * Created by cem on 27/10/16.
+ */
+public interface Neighbourhood {
+    List<KPMPSolution> getNeighbours(KPMPSolution solution);
+
+}
