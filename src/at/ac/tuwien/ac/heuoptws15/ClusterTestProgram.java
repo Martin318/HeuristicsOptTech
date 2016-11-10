f2477bbebdd520fa08bae1a977ced1f6dc63dd67
Mivey <mivster@ymail.com>
2016-11-10 16:35:21 +0100

    Renamed it for clarity

--- src/at/ac/tuwien/ac/heuoptws15/ClusterTestProgram.java
+++ src/at/ac/tuwien/ac/heuoptws15/ClusterTestProgram.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 /**
  * Created by Martin on 14.10.2016.
  */
-public class Main_OLD {
+public class ClusterTestProgram {
 
     public static void main(String[] args){
 
