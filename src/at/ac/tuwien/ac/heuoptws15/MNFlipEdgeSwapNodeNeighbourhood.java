6d4bb6d37b587257c8ee7c3fb79487c8dcc51b7a
Mivey <mivster@ymail.com>
2016-11-12 16:37:43 +0100

    added changes from N Edge FLip to MNFLipEdgebla

--- src/at/ac/tuwien/ac/heuoptws15/MNFlipEdgeSwapNodeNeighbourhood.java
+++ src/at/ac/tuwien/ac/heuoptws15/MNFlipEdgeSwapNodeNeighbourhood.java
@@ -8,14 +8,16 @@ public class MNFlipEdgeSwapNodeNeighbourhood  extends Neighbourhood {
     int N;
     int M;
     KPMPSolution orig_sol;
-    int pageindex[];
+    int pageIndex[];
     int pagesizes[];
     int transferIndex[];
+    int edgeSize;
+    int slotIndex[];        // The chosen edges at any given iteration
 
     int nodeTransferIndex[];
 
     public MNFlipEdgeSwapNodeNeighbourhood( int M, int N) {
-        this.M = M;
+        this.M = M+1;
         this.N = N;
     }
 
@@ -26,29 +28,32 @@ public class MNFlipEdgeSwapNodeNeighbourhood  extends Neighbourhood {
         // SET UP THE EDGE STUFF
 
 
-        int edgeSize = 0;
-        this.pagesizes = new int[orig_sol.pages.length];
-        int i = 0;
-        for (Page p : orig_sol.pages){
+        pagesizes = new int[orig_sol.pages.length];
+        edgeSize = 0;
+
+        for (int i = 0; i < orig_sol.pages.length;i++){
+            Page p = orig_sol.pages[i];
             edgeSize += p.edges.size();
-            if ( i > 0)
-                pagesizes[i] = pagesizes[i-1] + p.edges.size();
-            else
-                pagesizes[0] = p.edges.size();
-            i++;
+
+            pagesizes[i] = p.edges.size() + (( i > 0)?pagesizes[i-1]:0);
         }
+
         if (this.N > edgeSize)
             N = edgeSize;
+        pageIndex = new int[edgeSize];
         transferIndex = new int[N];
-        pageindex = new int[N];
+        slotIndex = new int[N];
 
 
-        int k = 0;
-        for( i = 0; i < N; i++){
+        for( int k = 0, i = 0; i < edgeSize; i++){
             if (i >= pagesizes[k])
                 k++;
-            pageindex[i] = k;    // CURRENT PAGE OF EDGE # (``NUMBER'') i
-            transferIndex[i] = ( k == 0)? 1 : 0;   // THE FIRST PAGE TO TRANSFER TO
+            pageIndex[i] = k;    // CURRENT PAGE OF EDGE # (``NUMBER'') i
+        }
+
+        for( int i = 0; i < N; i++){
+            slotIndex[i] = i;
+            transferIndex[i] = ( i <  pagesizes[0])? 1 : 0;   // THE FIRST PAGE TO TRANSFER TO
         }
 
         // SET UP THE NODE STUFF
@@ -58,7 +63,7 @@ public class MNFlipEdgeSwapNodeNeighbourhood  extends Neighbourhood {
 
         nodeTransferIndex = new int[M-1];
 
-        for( i = 0;  i < M-1; i++)
+        for( int i = 0;  i < M-1; i++)
             nodeTransferIndex[i] = i+1;
 
 
@@ -69,12 +74,15 @@ public class MNFlipEdgeSwapNodeNeighbourhood  extends Neighbourhood {
 
         // SET WHERE TO FLIP EDGES
 
-
+        int tempSlotIndex[] = new int[N];
         int tempTransferIndex[] = new int[N];
 
-        for( int i = 0;  i < N; i++)
-            tempTransferIndex[i] = RandomStuff.between(( pageindex[i] == 0)? 1 : 0,orig_sol.pages.length-1);
-
+        for( int i = 0;  i < N-1; i++){
+            tempSlotIndex[i] = RandomStuff.between( 0,edgeSize -1);
+            do{
+                tempTransferIndex[i] = RandomStuff.between( 0,orig_sol.pages.length-1);
+            } while ( tempTransferIndex[i] == pageIndex[tempSlotIndex[i]]);
+        }
 
         // SET WHICH NODES TO SWAP
 
@@ -112,14 +120,11 @@ public class MNFlipEdgeSwapNodeNeighbourhood  extends Neighbourhood {
         // EDGE BIT
 
         for(int i = 0; i < N; i++){
-            int index;
-            if ( pageindex[i] > 0)
-                index = i - pagesizes[pageindex[i]-1];
-            else
-                index = i;
-            Edge original = orig_sol.pages[pageindex[i]].edges.get(index);
+            int currentPage = pageIndex[tempSlotIndex[i]];
+            int index = tempSlotIndex[i] - ((currentPage > 0)?pagesizes[currentPage-1]:0);
+            Edge original = orig_sol.pages[currentPage].edges.get(index);
             Edge e = new Edge(original.getNameOfFirstVertex(),original.getNameOfSecondVertex());
-            solution.removeEdge(e,pageindex[i]);
+            solution.removeEdge(e,currentPage);
             solution.addEdge(e,tempTransferIndex[i]);
         }
 
@@ -132,9 +137,9 @@ public class MNFlipEdgeSwapNodeNeighbourhood  extends Neighbourhood {
         if(orig_sol.pages.length == 1)
             return null; // NO edge swap possible
 
-        // TERMINATION criterium EDGE
+        // TERMINATION CRITERIUM EDGE
 
-        if(transferIndex[0] >= orig_sol.pages.length )
+        if(slotIndex[0] >= edgeSize )
             return null;
 
         // TERMINATION CRITERIUM NODE
@@ -169,20 +174,29 @@ public class MNFlipEdgeSwapNodeNeighbourhood  extends Neighbourhood {
         // EDGE BIT
 
         for(int i = 0; i < N; i++){
-            Edge original = orig_sol.pages[pageindex[i]].edges.get(i);
+            int currentPage = pageIndex[slotIndex[i]];
+            int index = slotIndex[i] - ((currentPage > 0)?pagesizes[currentPage-1]:0);
+            Edge original = orig_sol.pages[currentPage].edges.get(index);
             Edge e = new Edge(original.getNameOfFirstVertex(),original.getNameOfSecondVertex());
-            solution.removeEdge(e,pageindex[i]);
+            solution.removeEdge(e,currentPage);
             solution.addEdge(e,transferIndex[i]);
         }
 
         // UPDATE THE INDICES TO INDICATE THE NEXT ELEMENT IN THE ORDERING W.R.T. THE ORIGINAL SOLUTION
 
 
-        for( int i = N-1;  i >= 0 ;i-- ){
+        for( int i = N-1;  i >= 0 ;i-- ){       // only iterated if both transfer and slot are reset
             transferIndex[i]++;
-
-            if (i != 0 && transferIndex[i] >= orig_sol.pages.length)
-                transferIndex[i] = ( pageindex[i] == 0)? 1 : 0;
+            if (transferIndex[i] == pageIndex[slotIndex[i]]) // skip edge's current page
+                transferIndex[i]++;
+            if (transferIndex[i] >= orig_sol.pages.length){  // transferIndex reset
+                transferIndex[i] = ( pageIndex[slotIndex[i]] == 0)? 1 : 0;
+                slotIndex[i]++;
+                if (i != 0 && slotIndex[i] >= edgeSize)          // slotIndex reset, not done at i = 0 to trigger termination
+                    slotIndex[i] = i;
+                else
+                    break;
+            }
             else
                 break;
         }
