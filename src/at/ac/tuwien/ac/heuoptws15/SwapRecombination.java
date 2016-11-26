76fed2101adcda637dd416985dc4c7c882fe3ecd
Mivey <mivster@ymail.com>
2016-11-26 14:53:04 +0100

    added mechanism to change ordering to SwapRecombination

--- src/at/ac/tuwien/ac/heuoptws15/SwapRecombination.java
+++ src/at/ac/tuwien/ac/heuoptws15/SwapRecombination.java
@@ -35,14 +35,21 @@ public class SwapRecombination extends  RecombinationOperator {
     }
 
     private KPMPSolution mating(KPMPSolution parentOne, KPMPSolution parentTwo){
-        KPMPSolution chosen;
+//        KPMPSolution chosen;
+//        if (Math.random() > 0.5)
+//            chosen = parentOne;
+//        else
+//            chosen = parentTwo;
+//        Integer[] ordering = new Integer[chosen.ordering.length];
+//        for(int i = 0; i < chosen.ordering.length;i++)
+//            ordering[i] = new Integer(chosen.ordering[i]);
+
+
+        Integer[] ordering;
         if (Math.random() > 0.5)
-            chosen = parentOne;
+            ordering = cycleSwap(parentOne.ordering,parentTwo.ordering);
         else
-            chosen = parentTwo;
-        Integer[] ordering = new Integer[chosen.ordering.length];
-        for(int i = 0; i < chosen.ordering.length;i++)
-            ordering[i] = new Integer(chosen.ordering[i]);
+            ordering = cycleSwap(parentTwo.ordering,parentOne.ordering);
 
         KPMPSolution child = new KPMPSolution(parentOne.ordering.length,parentOne.pages.length,ordering);
 
@@ -81,6 +88,49 @@ public class SwapRecombination extends  RecombinationOperator {
 
 
         return  child;
+    }
+
+
+    /**
+     *  Detects the first permutation cycle from mapping from ordering A to ordering B and applies it to orderingA
+     *
+     * @param orderingA
+     * @param orderingB
+     * @return  the ordering resulting from mapping the first permutation cycle to ordering A
+     */
+    private Integer[] cycleSwap(Integer[] orderingA, Integer[] orderingB){
+
+        Integer[] orderingCompA = new Integer[orderingA.length];
+        for (int i = 0; i < orderingA.length; i++)
+            orderingCompA[orderingA[i]] = i;
+
+        Integer[] swap = Arrays.copyOf(orderingA,orderingA.length);
+
+        //find start of possible cycle
+        int positionA = orderingA[0];
+        int positionB= orderingB[0];
+
+        int i = 0;
+        while( i < orderingA.length-1 && positionA == positionB  ){
+            i++;
+            positionA = orderingA[i];
+            positionB = orderingB[i];
+        }
+
+        if (orderingA[i] == orderingB[i])  // orderings are identical
+            return orderingA;
+
+        int begin = positionA;
+
+        do{
+            swap[i] = positionB;
+            i = orderingCompA[positionB];
+            positionB = orderingB[i];
+        } while ( ( begin != positionB));
+        swap[i] = begin;
+
+
+        return swap;
     }
 
 }
